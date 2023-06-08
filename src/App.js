3fa66d0857bb1f502d3c7b158aa21d2e12d490ac
Seval Senturk <seval.senturk@tgs.aero>
2023-06-08 10:13:04 +0300

    calculate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import "./App.css";
+import React, { useReducer } from "react";
+import Calculate from "./Calculate";
 
-import './App.css';
-import {useReducer} from 'react';
+export const NumberContext = React.createContext();
+
+const initialValue = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialValue;
+    default:
+      return state;
+  }
+};
 
 function App() {
- useReducer(reducer,initialValue)
+  const [count, dispatch] = useReducer(reducer, initialValue);
   return (
     <div className="App">
-        <button>Arttır</button>
-        <button>Azalt</button>
-        <button>Sıfırla</button>
+      <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
+        <Calculate />
+      </NumberContext.Provider>
     </div>
   );
 }
---
+++ src/Calculate.js
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { NumberContext } from "./App";
+
+function Calculate() {
+  const numberContext = useContext(NumberContext);
+  return (
+    <>
+      <div className="sayi">Sayi = {numberContext.count} </div>
+      <button onClick={() => numberContext.dispatch("increment")}>Arttır</button>
+      <button onClick={() => numberContext.dispatch("decrement")}>Azalt</button>
+      <button onClick={() => numberContext.dispatch("reset")}>Sıfırla</button>
+    </>
+  );
+}
+
+export default Calculate;
